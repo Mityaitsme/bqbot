031dbf8ecfa8e41f883c7fbcf34c3e2eb206928a
Yakir Dmitry <iakir.ds@phystech.edu>
2025-12-18 07:28:24 +0300

    Added some more settings, example for the .env file (hidden settings) and .gitignore

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -10,5 +10,15 @@ __all__ = [
     "BOT_TOKEN",
     "ADMIN",
     "STAGE_COUNT",
-    "DATABASE_URL"
+    "DATABASE_URL",
+    "START_TIME",
+
+    "CACHE_SIZE",
+    "TEAM_CACHE_SIZE",
+    "RIDDLE_CACHE_SIZE",
+    "MEMBER_CACHE_SIZE",
+
+    "TEAM_TABLE_NAME",
+    "MEMBER_TABLE_NAME",
+    "RIDDLE_TABLE_NAME"
 ]
--- src/config/settings.py
+++ src/config/settings.py
@@ -6,13 +6,10 @@ load_dotenv()
 
 # Telegram
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-ADMIN: int = int(os.getenv("ADMIN_ID", "0"))
+ADMIN: int = int(os.getenv("ADMIN", "0"))
 
 # Database
 DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-
-# Other
-STAGE_COUNT: int = 17
 CACHE_SIZE: int = 50
 TEAM_CACHE_SIZE: int = 50
 RIDDLE_CACHE_SIZE: int = 50
@@ -20,4 +17,7 @@ MEMBER_CACHE_SIZE: int = 100
 TEAM_TABLE_NAME: str = "team"
 MEMBER_TABLE_NAME: str = "member"
 RIDDLE_TABLE_NAME: str = "riddle"
-START_TIME: int = int(os.getenv("START_TIME", "1701369600"))
+
+# Other
+STAGE_COUNT: int = 17
+START_TIME: int = 1701369600
